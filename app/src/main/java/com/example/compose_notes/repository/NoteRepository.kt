898d7bf1e10698c90ef1db0c086f2bbcc9c4a316
Atul1223 <a1atulk@gmail.com>
2023-08-13 13:52:28 +0530

    Added Repository class
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

---
+++ app/src/main/java/com/example/compose_notes/repository/NoteRepository.kt
@@ -0,0 +1,32 @@
+package com.example.compose_notes.repository
+
+import com.example.compose_notes.data.NotesDatabaseDao
+import com.example.compose_notes.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(
+    private val notesDatabaseDao: NotesDatabaseDao
+) {
+    suspend fun addNote(note: Note) {
+        notesDatabaseDao.insertNote(note = note)
+    }
+
+    suspend fun updateNote(note: Note) {
+        notesDatabaseDao.updateNote(note = note)
+    }
+
+    suspend fun deleteNote(note: Note) {
+        notesDatabaseDao.deleteNote(note = note)
+    }
+
+    suspend fun deleteAllNotes() {
+        notesDatabaseDao.deleteAllNotes()
+    }
+
+    fun getAllNotes(): Flow<List<Note>> =
+        notesDatabaseDao.getNotes().flowOn(Dispatchers.IO).conflate() 
+}
