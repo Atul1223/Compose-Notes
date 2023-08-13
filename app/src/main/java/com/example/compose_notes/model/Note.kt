e7a361e142ee28ccc5a6c572ac891f2692f2334e
Atul1223 <a1atulk@gmail.com>
2023-08-13 12:40:58 +0530

    Created Notes Entity
    Signed-off-by: Atul1223 <a1atulk@gmail.com>

--- app/src/main/java/com/example/compose_notes/model/Note.kt
+++ app/src/main/java/com/example/compose_notes/model/Note.kt
@@ -1,12 +1,24 @@
 package com.example.compose_notes.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Calendar
 import java.util.Date
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note(
+
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "note_title")
     val title: String,
+
+    @ColumnInfo(name = "note_description")
     val description: String,
+
+    @ColumnInfo(name = "note_entry_date")
     val entryDate: Date = Calendar.getInstance().time
 )
